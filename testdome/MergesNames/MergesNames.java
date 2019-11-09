51008a2007edc7fd29ebdcb0e9186dd547e3a4d5
matigoal <55578343+matigoal@users.noreply.github.com>
2019-11-09 19:44:23 +0100

    Update MergesNames.java

--- testdome/MergesNames/MergesNames.java
+++ testdome/MergesNames/MergesNames.java
@@ -1,16 +1,13 @@
 import java.util.Arrays; //importation Java Array class java.util.Arrays usage des listes
 import java.util.HashSet; //importation Java HashSet util.HashSet class  mécanisme de hachage
 import java.util.Set; //importation jav.util.Set  empêche les doublons
-
 //déclaration de la méthode
 public class MergeNames {
-
     public static String[] uniqueNames(String[] listnames1, String[] listnames2) {
         Set<String> listnames = new HashSet<>(Arrays.asList(listnames1));
         listnames.addAll(Arrays.asList(listnames2));
         return listnames.toArray(new String[0]);
     }
-    
     public static void main(String[] args) {
         String[] listnames1 = new String[] {"Ava", "Emma", "Olivia"};//list 1
         String[] listnames2 = new String[] {"Olivia", "Sophia", "Emma"};//list 2
